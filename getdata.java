3b588d2428d838f89238f7853a048399cad20761
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-10-27 19:13:07 +0100

    Add comments

--- getdata.java
+++ getdata.java
@@ -1,17 +1,17 @@
 package com.github;
 
-import java.net.URL;
+import java.net.URL; // import package to use URL and methods
 
 public class URLGitHub {
     public static void main(String[] args) {
         try {
             URL url = new URL("https://github.com/marketplace");
-            System.out.println("Protocol: " + url.getProtocol());
-            System.out.println("Host: " + url.getHost());
-            System.out.println("Path: " + url.getPath());
-            System.out.println("Port: " + url.getPort());
+            System.out.println("Protocol: " + url.getProtocol()); // get protocol
+            System.out.println("Host: " + url.getHost()); // get host
+            System.out.println("Path: " + url.getPath()); // get path
+            System.out.println("Port: " + url.getPort()); // get port
         } catch (Exception error) {
-            System.out.println(error);
+            System.out.println(error); // throw an error in case request fails
         }
     }
 }
